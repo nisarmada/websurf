d503ed5dca07a4e22f2c2a098c087b8899fbbe7b
Rupsenjager <146854614+Rupsenjager@users.noreply.github.com>
2025-06-09 11:19:36 +0200

    parsing start

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: snijhuis <snijhuis@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/06 14:27:06 by snijhuis      #+#    #+#                 */
-/*   Updated: 2025/06/06 17:44:42 by snijhuis      ########   odam.nl         */
+/*   Updated: 2025/06/08 19:36:08 by stannijhuis   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,56 +17,83 @@
 #include <unistd.h>      // For close()
 #include <cstring>       // For memset()
 #include <iostream>      // For std::cout, std::cerr>
+#include <fstream>
 
 //TCP is Transmission control protocol
 //check out epoll
 
-int main (void)
+int main (int argc, char** argv)
 {
-    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if(socket_fd == -1)
+    if(argc != 2)
     {
-        std::cerr << "Error: Socket() failed" << std::endl;
+        std::cerr << "Usage: ./webserv configfile" << std::endl;
         return 1;
     }
-    
-    sockaddr_in address{}; //creates struct initialised everything to 0
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY; //listens to all network interfaces
-    address.sin_port = htons(PORT); //changes the way numbers are stored (computer and network store differnt.)
-    
-    if(bind(socket_fd, reinterpret_cast<sockaddr*>(&address), sizeof(address)) == -1) //binds the address information to the socket otherwise it has no information and does nothing
+    std::ifstream configFile(argv[1]);
+    if(!configFile.is_open())
     {
-        std::cerr << "Error: bind() failed" << std::endl;
+        std::cerr << "Error could not open the config file." << std::endl;
         return 1;
     }
-    if(listen(socket_fd, BACKLOG) == -1) //it won't send or rceive data directly it will wait for clients to connect
+
+    std::string line;
+    while(std::getline(configFile, line))
     {
-        std::cerr << "Error: listen() failed" << std::endl;
-        return 1;
+        line.erase(0, line.find_first_not_of(" \t\n"));
+        std::cout << line << std::endl;
     }
-    std::cout << "Server is listening on a port: " << PORT << std::endl;
-    
-    sockaddr_in client_address{};
-    socklen_t client_len = sizeof(client_address); //socklen_t is unsigned int
-    
-    int client_socket = accept(socket_fd, reinterpret_cast<sockaddr*>(&client_address), &client_len); //creates a socket for the client to connect to so the main socket can remain open.
-    
-    char buffer[1024] = {0};
-    int bytes_received = read(client_socket, buffer, sizeof(buffer) - 1); //reads from connected client stores in buffer and returns how many bytes received
-
-    std::cout << "Received " << bytes_received << " bytes\n";
-
-    std::string response =
-    "HTTP/1.1 200 OK\r\n"
-    "Content-Type: text/html\r\n"
-    "Content-Length: 13\r\n"
-    "\r\n"
-    "Hello, world!"; //HTTP hard coded to send hello world.
-
-    send(client_socket, response.c_str(), response.size(), 0); //sending the response, send expects c style. 0 stands for default behaviour.
-
-    close(client_socket);
-    close(socket_fd);
     return 0;
 }
+//removing newlines double spaces etc. cleanup before the next step.
+
+
+
+// int main (void)
+// {
+//     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+//     if(socket_fd == -1)
+//     {
+//         std::cerr << "Error: Socket() failed" << std::endl;
+//         return 1;
+//     }
+    
+//     sockaddr_in address{}; //creates struct initialised everything to 0
+//     address.sin_family = AF_INET;
+//     address.sin_addr.s_addr = INADDR_ANY; //listens to all network interfaces
+//     address.sin_port = htons(PORT); //changes the way numbers are stored (computer and network store differnt.)
+    
+//     if(bind(socket_fd, reinterpret_cast<sockaddr*>(&address), sizeof(address)) == -1) //binds the address information to the socket otherwise it has no information and does nothing
+//     {
+//         std::cerr << "Error: bind() failed" << std::endl;
+//         return 1;
+//     }
+//     if(listen(socket_fd, BACKLOG) == -1) //it won't send or rceive data directly it will wait for clients to connect
+//     {
+//         std::cerr << "Error: listen() failed" << std::endl;
+//         return 1;
+//     }
+//     std::cout << "Server is listening on a port: " << PORT << std::endl;
+    
+//     sockaddr_in client_address{};
+//     socklen_t client_len = sizeof(client_address); //socklen_t is unsigned int
+    
+//     int client_socket = accept(socket_fd, reinterpret_cast<sockaddr*>(&client_address), &client_len); //creates a socket for the client to connect to so the main socket can remain open.
+    
+//     char buffer[1024] = {0};
+//     int bytes_received = read(client_socket, buffer, sizeof(buffer) - 1); //reads from connected client stores in buffer and returns how many bytes received
+
+//     std::cout << "Received " << bytes_received << " bytes\n";
+
+//     std::string response =
+//     "HTTP/1.1 200 OK\r\n"
+//     "Content-Type: text/html\r\n"
+//     "Content-Length: 13\r\n"
+//     "\r\n"
+//     "Hello, world!"; //HTTP hard coded to send hello world.
+
+//     send(client_socket, response.c_str(), response.size(), 0); //sending the response, send expects c style. 0 stands for default behaviour.
+
+//     close(client_socket);
+//     close(socket_fd);
+//     return 0;
+// }
